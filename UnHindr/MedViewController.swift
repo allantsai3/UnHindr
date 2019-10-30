4640a5c20813f4aed52f8a50339c0a6083f97736
Jordan Kam <jdkam@asb9700-e05.csil.sfu.ca>
2019-10-29 19:48:46 -0700

    Created storyboard and view controller for medication functionality, started work on both

---
+++ UnHindr/MedViewController.swift
@@ -0,0 +1,12 @@
+//
+//  MedViewController.swift
+//  UnHindr
+//
+//  Created by Jordan Kam on 10/29/19.
+//  Copyright © 2019 Sigma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
---
+++ UnHindr/ViewControllers/MedicationViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MedicationViewController.swift
+//  UnHindr
+//
+//  Created by Jakeb Puffer  on 10/29/19.
+//  Copyright © 2019 Sigma. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MedicationViewController: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+}
