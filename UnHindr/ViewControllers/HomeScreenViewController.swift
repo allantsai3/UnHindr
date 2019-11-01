a132d0db077970b9a55aef04db23ab2937ddce54
ata87 <ata87@sfu.ca>
2019-11-01 10:38:42 -0700

    login authentication with firstname/lastname message on login

--- UnHindr/ViewControllers/HomeScreenViewController.swift
+++ UnHindr/ViewControllers/HomeScreenViewController.swift
@@ -8,12 +8,54 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseFirestore
 
 class HomeScreenViewController : UIViewController {
+    
+    // Handler for the auth state listener
+    private var handle: AuthStateDidChangeListenerHandle?
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Start auth listener
+        handle = Auth.auth().addStateDidChangeListener({ (auth, user) in
+            self.setDisplayName(user)
+        })
+    }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        Auth.auth().removeStateDidChangeListener(handle!)
+    }
+    
+    private func setDisplayName(_ user: User?){
+        let uid = user?.uid ?? "User instance is nil"
+        //Retrieve first and last name
+        let db = Firestore.firestore()
+        db.collection("users").whereField("uid", isEqualTo: uid)
+            .getDocuments() { (querySnapshot, err) in
+                if let err = err {
+                    print("Error getting document: \(err)")
+                } else {
+                    // should only have 1 document
+                    for document in querySnapshot!.documents {
+                        let firstName = (document.get("firstName") ?? "" ) as? String
+                        let lastName = (document.get("lastName") ?? "") as? String
+                        let name = firstName! + " " + lastName!
+                        self.nameLabel.text = name
+//                        print("\(document.documentID) => \(document.data())")
+//                        print(document.get("lastName") ?? "")
+                    }
+                }
+        }
+    }
     
 }
