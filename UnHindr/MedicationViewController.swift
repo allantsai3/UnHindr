0fed762677c8562fe79d82f5bcbf667536008bcb
jjpuffer <jjpuffer@asb9700-e04.csil.sfu.ca>
2019-11-01 17:10:39 -0700

    Added functionality for input of medication name and time for medication notifications (to be implemented in v2)

--- UnHindr/MedicationViewController.swift
+++ UnHindr/MedicationViewController.swift
@@ -21,6 +21,10 @@ class MedicationViewController: UIViewController {
     @IBOutlet weak var checkSat: UIImageView!
     @IBOutlet weak var checkSun: UIImageView!
 
+    @IBOutlet weak var medFieldName: UITextField!
+    
+    private var timePicker: UIDatePicker?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -31,8 +35,39 @@ class MedicationViewController: UIViewController {
         checkFri.alpha = 0
         checkSat.alpha = 0
         checkSun.alpha = 0
+        
+        configureMedNameText()
+        configureTapGesture()
+        
+        timePicker = UIDatePicker()
+        timePicker?.datePickerMode = .time
+        timePicker?.addTarget(self, action: #selector(MedicationViewController.timeChanged(timePicker:)), for: .valueChanged)
+        
+        timeInputTextField.inputView = timePicker
+    }
+    
+
+    
+    @objc func timeChanged(timePicker: UIDatePicker) {
+        let timeFormatter = DateFormatter()
+        timeFormatter.dateFormat = "hh:mm"
+        timeInputTextField.text = timeFormatter.string(from: timePicker.date)
+    }
+    
+    @IBOutlet weak var timeInputTextField: UITextField!
+    
+    private func configureTapGesture() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(MedicationViewController.handleTapOutsideKeyboard))
+        view.addGestureRecognizer(tapGesture)
     }
 
+    @objc func handleTapOutsideKeyboard() {
+        view.endEditing(true)
+    }
+    
+    private func configureMedNameText(){
+        medFieldName.delegate = self
+    }
     @IBAction func monButtonTapped(_ sender: Any) {
         if (checkMon.alpha == 1){
             checkMon.alpha = 0
@@ -40,6 +75,7 @@ class MedicationViewController: UIViewController {
         else {
             checkMon.alpha = 1
         }
+        view.endEditing(true)
     }
     @IBAction func tuesButtonTapped(_ sender: Any) {
         if (checkTues.alpha == 1){
@@ -48,6 +84,7 @@ class MedicationViewController: UIViewController {
         else {
             checkTues.alpha = 1
         }
+        view.endEditing(true)
     }
     @IBAction func wedButtonTapped(_ sender: Any) {
         if (checkWed.alpha == 1){
@@ -56,6 +93,7 @@ class MedicationViewController: UIViewController {
         else {
             checkWed.alpha = 1
         }
+        view.endEditing(true)
     }
     @IBAction func thursButtonPressed(_ sender: Any) {
         if (checkThur.alpha == 1){
@@ -64,6 +102,7 @@ class MedicationViewController: UIViewController {
         else {
             checkThur.alpha = 1
         }
+        view.endEditing(true)
     }
     @IBAction func friButtonPressed(_ sender: Any) {
         if (checkFri.alpha == 1){
@@ -72,6 +111,7 @@ class MedicationViewController: UIViewController {
         else {
             checkFri.alpha = 1
         }
+        view.endEditing(true)
     }
     @IBAction func satButtonPressed(_ sender: Any) {
         if (checkSat.alpha == 1){
@@ -80,6 +120,7 @@ class MedicationViewController: UIViewController {
         else {
             checkSat.alpha = 1
         }
+        view.endEditing(true)
     }
     @IBAction func sunButtonPressed(_ sender: Any) {
         if (checkSun.alpha == 1){
@@ -88,37 +129,56 @@ class MedicationViewController: UIViewController {
         else {
             checkSun.alpha = 1
         }
+        view.endEditing(true)
     }
     @IBAction func addMedTapped(_ sender: Any) {
         //send data
+        view.endEditing(true)
     }
     @IBAction func cancelMedTapped(_ sender: Any) {
         //go back to med screen
+        view.endEditing(true)
     }
     @IBOutlet weak var dosageLabel: UILabel!
     @IBAction func dosageStepper(_ sender: UIStepper) {
         dosageLabel.text = String(Int(sender.value))
+        view.endEditing(true)
     }
     
     @IBOutlet weak var quantityLabel: UILabel!
     @IBAction func quantityStepper(_ sender: UIStepper) {
         quantityLabel.text = String(Int(sender.value))
+        view.endEditing(true)
     }
+
 }
 
+extension MedicationViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
+/*
+extension UIToolbar {
     
+    func ToolbarPicker(mySelect : Selector) -> UIToolbar {
+        
+        let toolBar = UIToolbar()
+        
+        toolBar.barStyle = UIBarStyle.default
+        toolBar.isTranslucent = true
+        toolBar.tintColor = UIColor.black
+        toolBar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(title: "Done", style: UIBarButtonItem.Style.plain, target: self, action: mySelect)
+        let spaceButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil)
+        
+        toolBar.setItems([ spaceButton, doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        
+        return toolBar
+    }
     
-    // @IBAction func timeButtonPressed(_ sender: Any) {
-        /*timePicker.datePickerMode = .time
-        timePicker.frame = CGRect(x: 0.0, y: (self.view.frame.height/2 + 60), width: self.view.frame.width, height: 150.0)
-        timePicker.backgroundColor = UIColor.white
-        self.view.addSubview(timePicker)
-        timePicker.addTarget(self, action: #selector(MedicationViewController.timeChanged), for: UIControl.Event.valueChanged)*/
-    //}
-   // @objc func timeChanged(sender: UIDatePicker) {
-    //    let formatter = DateFormatter()
-     //   formatter.timeStyle = .short
-        //timeButtonPressed = formatter.string(from: sender.date)
-    //    timePicker.removeFromSuperview() // if you want to remove time picker
-    //}//
-//}
+}
+*/
